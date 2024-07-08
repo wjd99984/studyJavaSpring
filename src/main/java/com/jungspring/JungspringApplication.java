040b136a195fbb5b6ed8daf886335e227df31194
jung tae wook <xodnr901901@naver.com>
2024-07-08 15:52:33 +0900

    첫커밋

---
+++ src/main/java/com/jungspring/JungspringApplication.java
@@ -0,0 +1,13 @@
+package com.jungspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungspring/JungspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
